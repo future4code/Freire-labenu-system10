ad663edd9f79618e1e75fce89d33817c55ec123a
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-09-06 20:46:06 -0300

    base do projeto com types feito

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type Turma = {
+    id: string,
+    nome: string,
+    docentes: Array<Docente>,
+    estudantes: Array<Estudante>,
+    modulo: 0 | 1 | 2 | 3 | 4 | 5 | 6
+}
+
+export type Estudante = {
+    id: string,
+    nome: string,
+    email: string,
+    data_nasc: string,
+    turma_id: string
+}
+
+export type Docente = {
+    id: string,
+    nome: string,
+    email: string,
+    data_nasc: string,
+    turma_id: string
+}
